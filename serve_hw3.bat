24dc32d912633e7d79e977321502e37d46b6650d
wenjingwang99 <Githubsuccess66>
2022-02-22 19:24:00 -0500

    update server

---
+++ serve_hw3.bat
@@ -0,0 +1,4 @@
+cd C:\Users\vcm\PycharmProjects\homework_3-1
+git pull https://%TESTAPP_GIT_PAT%@https://github.com/wenjingwang99/homework_3-1
+venv\Scripts\python.exe -m pip install -r requirements.txt
+venv\Scripts\python.exe server.py
